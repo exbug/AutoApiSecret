5e278b47c3eca5732ebe3bf63082254cc6c469c2
exbug <10230528+exbug@users.noreply.github.com>
2021-01-23 22:06:20 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -22,9 +22,8 @@ def gettoken(refresh_token):
           'refresh_token': refresh_token,
           'client_id':id,
           'client_secret':secret,
-          'redirect_uri':'http://localhost:53682/'
+          'redirect_uri':'https://heymind.github.io/tools/microsoft-graph-api-auth'
          }
-    print("data:"+str(data))
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
