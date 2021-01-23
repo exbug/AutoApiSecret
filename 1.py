f96001c20f16b32aaf5c59e4d402afe4d92f13a7
exbug <10230528+exbug@users.noreply.github.com>
2021-01-23 22:04:29 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -24,6 +24,7 @@ def gettoken(refresh_token):
           'client_secret':secret,
           'redirect_uri':'http://localhost:53682/'
          }
+    print("data:"+str(data))
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
